8ca0c00aab0473236a8f84e6f71aa3fb8e2aad0c
birsenaydin <bsnaydin@gmail.com>
2025-05-05 09:54:21 +0100

    chore: Initialize BankManagementSystem project

---
+++ main.go
@@ -0,0 +1,18 @@
+// main.go
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	router.Run(":8080") // Uygulama localhost:8080'de çalışır
+}
